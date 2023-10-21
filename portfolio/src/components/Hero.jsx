383e6627dd2e7193eca35f87a34f3d91e7e4f325
Arya Rajput <rajputarya10@gmail.com>
2023-10-21 18:03:25 +0530

    Basic setup

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="bg-white min-h-screen flex justify-center items-center">
+        <div className="flex items-center flex-col space-y-1 p-8 ">
+            <h1  className="text-3xl text-green-700 font-bold font-serif">
+                Arya Rajput
+            </h1>
+            <p className="text-xl font-semibold text-gray-800">
+                UI Developer
+            </p>
+            <div className="max-w-xl text-center text-sm text-gray-600">
+            I am a web developer. I am proficient in React and Vue, and have a strong understanding of front-end development principles and practices.
+            </div>
+        </div>
+    </div>
+  )
+}
