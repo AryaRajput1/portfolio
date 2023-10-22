3c543bec711e2b85af094502b33c39a2488586c4
Arya Rajput <rajputarya10@gmail.com>
2023-10-22 15:08:54 +0530

    added links and resume

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
       <nav className="absolute top-0 left-0 right-0 h-16 border flex justify-end items-center p-8">
-            <a className="py-2 px-9 bg-green-700 text-white duration-200 hover:border rounded-md text-bold border-green-800 hover:bg-white hover:text-green-700 ">Resume</a>
+            <a href="https://drive.google.com/file/d/1P2-MdTwZHR3-9TkBMvT6SQAE7zDxOabS/view?usp=share_link" target="_blank" rel="noreferrer" className="py-2 px-9 bg-green-700 text-white duration-200 hover:border rounded-md text-bold border-green-800 hover:bg-white hover:text-green-700 ">Resume</a>
     </nav>
   )
 }
