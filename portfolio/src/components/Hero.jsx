d561cbef14a73361d45b9f8a184650596080a466
Arya Rajput <rajputarya10@gmail.com>
2023-10-22 15:12:37 +0530

    updates

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,26 +1,32 @@
-import { BsGithub,BsLinkedin,BsInstagram } from 'react-icons/bs';
+import { BsGithub, BsLinkedin, BsInstagram } from "react-icons/bs";
 
 export default function Hero() {
   return (
     <div className="bg-white min-h-screen flex justify-center items-center">
-        <div className='flex flex-col space-y-4'>
+      <div className="flex flex-col space-y-4">
         <div className="flex items-center flex-col space-y-1 p-8 ">
-            <h1  className="text-3xl text-green-700 font-bold font-serif">
-                Arya Rajput
-            </h1>
-            <p className="text-xl font-semibold text-gray-800">
-                UI Developer
-            </p>
-            <div className="max-w-xl text-center text-sm text-gray-600">
-            I am a web developer. I am proficient in React and Vue, and have a strong understanding of front-end development principles and practices.
-            </div>
+          <h1 className="text-3xl text-green-700 font-bold font-serif">
+            Arya Rajput
+          </h1>
+          <p className="text-xl font-semibold text-gray-800">UI Developer</p>
+          <div className="max-w-xl text-center text-sm text-gray-600">
+            I am a web developer. I am proficient in React and Vue, and have a
+            strong understanding of front-end development principles and
+            practices.
+          </div>
         </div>
         <div className="flex space-x-6 justify-center items-center text-4xl text-green-700">
-            <a href='https://github.com/AryaRajput1'><BsGithub/></a>
-            <a href='https://www.linkedin.com/in/aryarajput/'><BsLinkedin/></a>
-            <a href='https://www.instagram.com/_rajarya/'><BsInstagram/></a>
-            </div>
+          <a href="https://github.com/AryaRajput1">
+            <BsGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/aryarajput/">
+            <BsLinkedin />
+          </a>
+          <a href="https://www.instagram.com/_rajarya/">
+            <BsInstagram />
+          </a>
         </div>
+      </div>
     </div>
-  )
+  );
 }
