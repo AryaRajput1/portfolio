1489a09693003e06b892eeaa31179a39d38b96f4
Arya Rajput <rajputarya10@gmail.com>
2023-10-28 14:39:58 +0530

    fix image paths

--- portfolio/public/data/projectList.js
+++ portfolio/public/data/projectList.js
@@ -1,6 +1,6 @@
 export const projectList = [
     {
-        imgLink: '../../public/images/currency-converter.png',
+        imgLink: '/images/currency-converter.png',
         title:'Currency Converter',
         link:'https://ar-currency-converter.netlify.app',
         bannerTitle:'ReactJs'
