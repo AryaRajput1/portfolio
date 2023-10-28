010ea82ddaee8f9a75a22785e7dd13f3a47b951d
Arya Rajput <rajputarya10@gmail.com>
2023-10-28 15:55:54 +0530

    added todo project

--- portfolio/public/data/projectList.js
+++ portfolio/public/data/projectList.js
@@ -5,6 +5,19 @@ export const projectList = [
         link:'https://ar-currency-converter.netlify.app',
         bannerTitle:'ReactJs'
     },
+    {
+        imgLink: '/images/tailwind-1.png',
+        title:'Gallary Minor',
+        link:'https://ar-tailwind-project-1.netlify.app',
+        bannerTitle:'Tailwind'
+    },
+    {
+        imgLink: '/images/todo.png',
+        title:'DnD Todo',
+        link:'https://ar-todos.netlify.app',
+        bannerTitle:'ReactJs'
+    },
+
     
 ]
 
